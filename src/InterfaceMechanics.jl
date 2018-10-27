825661fabd2cc8934c5e093f8107e96c9d066e45
mantyant <mantyant@gmail.com>
2018-10-27 16:07:43 +0300

    Test added

--- src/InterfaceMechanics.jl
+++ src/InterfaceMechanics.jl
@@ -1,6 +1,11 @@
 module InterfaceMechanics
 
-println('ladataan paketti')
+export hello, domath
+
+hello(who::String) = "Hello, $who"
+domath(x::Number) = x + 5
+
+println("ladataan paketti")
 greet() = print("Hello World!")
 
 end # module
